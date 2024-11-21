74a7eb7c1a7733e92333785ab6ac7028b6fdbabe
Matthew Sheluga <matthew.sheluga@gmail.com>
2024-11-21 14:57:22 -0500

    redid the permissions since the generation lost it

--- Models/Address.cs
+++ Models/Address.cs
@@ -4,6 +4,7 @@ using System;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
+using HC14Test.Authorization;
 using Microsoft.EntityFrameworkCore;
 
 namespace HC14Test.Models;
@@ -21,6 +22,7 @@ public partial class Address
     /// Primary key for Address records.
     /// </summary>
     [Key]
+    [PermissionAuthorize("AdventureWorks2022.Person.Address.AddressID.Read")]
     public int AddressID { get; set; }
 
     /// <summary>
@@ -28,12 +30,14 @@ public partial class Address
     /// </summary>
     [Required]
     [StringLength(60)]
+    [PermissionAuthorize("AdventureWorks2022.Person.Address.AddressLine1.Read")]
     public string AddressLine1 { get; set; }
 
     /// <summary>
     /// Second street address line.
     /// </summary>
     [StringLength(60)]
+    [PermissionAuthorize("AdventureWorks2022.Person.Address.AddressLine2.Read")]
     public string AddressLine2 { get; set; }
 
     /// <summary>
@@ -41,11 +45,13 @@ public partial class Address
     /// </summary>
     [Required]
     [StringLength(30)]
+    [PermissionAuthorize("AdventureWorks2022.Person.Address.City.Read")]
     public string City { get; set; }
 
     /// <summary>
     /// Unique identification number for the state or province. Foreign key to StateProvince table.
     /// </summary>
+    [PermissionAuthorize("AdventureWorks2022.Person.Address.StateProvinceID.Read")]
     public int StateProvinceID { get; set; }
 
     /// <summary>
@@ -53,17 +59,20 @@ public partial class Address
     /// </summary>
     [Required]
     [StringLength(15)]
+    [PermissionAuthorize("AdventureWorks2022.Person.Address.PostalCode.Read")]
     public string PostalCode { get; set; }
 
     /// <summary>
     /// ROWGUIDCOL number uniquely identifying the record. Used to support a merge replication sample.
     /// </summary>
+    [PermissionAuthorize("AdventureWorks2022.Person.Address.rowguid.Read")]
     public Guid rowguid { get; set; }
 
     /// <summary>
     /// Date and time the record was last updated.
     /// </summary>
     [Column(TypeName = "datetime")]
+    [PermissionAuthorize("AdventureWorks2022.Person.Address.ModifiedDate.Read")]
     public DateTime ModifiedDate { get; set; }
 
     [InverseProperty("Address")]
