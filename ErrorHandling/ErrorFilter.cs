e6189bc6cb8cecc7f08b3f048516a373296fe844
Matthew Sheluga <matthew.sheluga@gmail.com>
2024-12-18 14:46:11 -0500

    updated Error Filter for example

--- ErrorHandling/ErrorFilter.cs
+++ ErrorHandling/ErrorFilter.cs
@@ -1,6 +1,32 @@
-﻿namespace HC14Test.ErrorHandling
+﻿using System.Collections.Concurrent;
+
+namespace HC14Test.ErrorHandling
 {
     public class ErrorFilter
     {
+
+        private readonly ConcurrentDictionary<string, IError> _errorDictionary = new();
+        /// <summary>
+        /// OnError is called whenever an error occurred during execution of a query.
+        /// </summary>
+        /// <param name="error">The error, cannot be null.</param>
+        /// <returns>The filtered error</returns>
+        public IError OnError(IError error)
+        {
+            var errorMessage = error.Message;
+            if (!_errorDictionary.ContainsKey(errorMessage))
+            {
+                _errorDictionary.TryAdd(errorMessage, error);
+                LogError(error);
+                return error;
+            }
+            return null;
+
+        }
+
+        private void LogError(IError error)
+        {
+            Console.WriteLine(error.Message);
+        }
     }
 }
